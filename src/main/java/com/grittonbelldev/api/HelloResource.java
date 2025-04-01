38f3d3fb0e9e886e69315099269a7536592f8861
Justin Gritton-Bell <jgrittonbell@madisoncollege.edu>
2025-04-01 12:36:19 -0500

    Added HelloWorld API demo for troubleshooting with Paula

--- src/main/java/com/grittonbelldev/api/HelloResource.java
+++ src/main/java/com/grittonbelldev/api/HelloResource.java
@@ -1,5 +1,8 @@
 package com.grittonbelldev.api;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -9,9 +12,14 @@ import javax.ws.rs.core.Response;
 @Path("/hello")
 public class HelloResource {
 
+
+    private final Logger logger = LogManager.getLogger(this.getClass());
+
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public Response sayHello() {
+        logger.info("Hello World");
         return Response.status(200).entity("Hello from Jersey!").build();
     }
 }
